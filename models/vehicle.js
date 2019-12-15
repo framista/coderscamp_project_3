f428b22763048a1480ad6726388fd853eb2ff079
framista <adajab1@gmail.com>
2019-12-15 18:34:16 +0100

    Added methods for vehicle

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,18 +1,20 @@
 const config = require('config');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
 const vehicleSchema = new mongoose.Schema({
-    number: {type: Number, required: true },    // some serial number
-    type: {type: String, required: true, 
+    number: { type: Number, required: true },    // some serial number
+    type: {
+        type: String, required: true,
         enum: ['Micro', 'Sedan', 'CUV', 'SUV', 'Coupe', 'Hatchback', 'Pickup',
-             'VAN', 'Campervan', 'Mini Truck', 'Minivan', 'Truck', 'Big Truck', 'Bus'] },   
-    brand: {type: String, required: true }, // like BMW
-    plate: {type: String, required: true },
-    productionYear: {type: Date, required: true},
-    isAvaliable: {type: Boolean, default: true },
-    fuelType: {type: String, required: true, enum:['Gasoline', 'Diesel'] },
-    routes: String
+            'VAN', 'Campervan', 'Mini Truck', 'Minivan', 'Truck', 'Big Truck', 'Bus']
+    },
+    brand: { type: String, required: true }, // like BMW
+    plate: { type: String, required: true },
+    productionYear: { type: Date, required: true },
+    isAvailable: { type: Boolean, default: true },
+    fuelType: { type: String, required: true, enum: ['Gasoline', 'Diesel'] },
+    vehicleRoutes: String
 });
 
 const Vehicle = mongoose.model('Vehicle', vehicleSchema);
